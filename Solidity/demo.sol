1303f2a19ce81049f9273de6cf5aa47a4808de82
orca123456 <orca123456@163.com>
2018-09-18 22:22:05 +0800

    -

--- Solidity/demo.sol
+++ Solidity/demo.sol
@@ -1,11 +1,33 @@
 pragma solidity ^0.4.0;
 
-import “solidity_for_import.sol”
+import "solidity_for_import.sol";
 
 // This is a Contract
 
 contract Test {
     uint a;
+    bool boola = true;
+    bool boolb = false;
+    int256 b = 20;
+    int256 c = 30;
+
+    function testadd() public returns (int) {
+        if (b>c) {
+            return b+c;
+        } else if (b==c){
+            return b * c;
+        } else{
+            return b >> 2;
+        }
+    }
+
+    function testbool() public returns (bool) {
+        return !boola;
+    }
+
+    function testbool() public returns (bool) {
+        return boola || boolb
+    }
 
     function setA(uint x) public {
         a = x;
