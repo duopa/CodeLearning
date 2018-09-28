bc52799b959cd84746c935aa3906a90e0381586b
orca123456 <orca123456@163.com>
2018-09-28 18:58:58 +0800

    -

--- py-DataAnalysis/A001.py
+++ py-DataAnalysis/A001.py
@@ -1,7 +1,18 @@
-import numpy
-import pandas
-import matplotlib
-import scipy
-import sklearn
-import keras
+import time
 
+def swap(a,b):
+    b,a=a,b
+
+
+a,b=3,4
+swap(a,b)
+print(a,b)
+
+a=0
+async def add(a):
+    a+=1
+    return print(a)
+
+
+for i in range(10):
+    add(a)
